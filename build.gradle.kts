ffb843dbba37a60a9b9935537407795df18bd68a
Adrien BOISSIER <33319289+trixidis@users.noreply.github.com>
2026-01-13 18:06:58 +0100

    chore: update gradle configuration and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,4 +9,38 @@ plugins {
     alias(libs.plugins.ktorfit) apply false
     alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.ktlint)
+}
+
+subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configure<io.gitlab.arturbosch.detekt.extensions.DetektExtension> {
+        config.setFrom(rootProject.files("config/detekt/detekt.yml"))
+        buildUponDefaultConfig = true
+        parallel = true
+    }
+
+    tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+        jvmTarget = "11"
+    }
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        version.set("1.5.0")
+        android.set(true)
+        outputToConsole.set(true)
+        ignoreFailures.set(false)
+        filter {
+            exclude { element -> element.file.path.contains("/generated/") }
+            exclude { element -> element.file.path.contains("/build/") }
+        }
+    }
+
+    tasks.withType<org.jlleitschuh.gradle.ktlint.tasks.GenerateReportsTask>().configureEach {
+        reportsOutputDirectory.set(
+            project.layout.buildDirectory.dir("reports/ktlint/$name")
+        )
+    }
 }
